2ae111633a79b06794da67e02864539fd648687b
gioeste <61016188+gioeste@users.noreply.github.com>
2023-05-31 14:45:17 +0200

    Update yourDetails.js

--- const/yourDetails.js
+++ const/yourDetails.js
@@ -1,2 +1,2 @@
 // Replace this with your staking contract address
-export const stakingContractAddress = "0x9fB6514bc5048C92c87f4d25371325758f30592A";
+export const stakingContractAddress = "0x55aD583343092d54499C1F951E53E856a915CD88";
